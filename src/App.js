fb001c44105899a70bba17f0e76dd3ba1c456740
Henrique Santi <phsantossantiago@hotmail.com>
2021-02-15 16:46:52 -0400

    Ready to go for Github

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is the first Koww developing page
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          See the Repo
         </a>
       </header>
     </div>
